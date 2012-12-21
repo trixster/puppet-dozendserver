a51ae9f34d85587927000df5f641406041be4df1
cleverlight <github.account@cleveraspaint.com>
2012-12-21 18:01:43 +0000

    commented LD_LIBRARY_PATH because it is breaking lib paths

--- files/zend.sh
+++ files/zend.sh
@@ -1,3 +1,5 @@
 # append zend to the path for all users
 export PATH=${PATH}:/usr/local/zend/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/zend/lib
+
+# setting LD_LIBRARY_PATH breaks lynx/atop/htop
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/zend/lib
