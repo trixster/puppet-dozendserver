e2b284cf0f947aefb3250ca1e2b860cc56aacccf
cleverlight <github.account@cleveraspaint.com>
2013-03-01 15:47:48 +0000

    minor formatting tweaks

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,55 +31,42 @@ class dozendserver (
       package { 'prelink' :
         ensure => 'present',
       }
-    # temporarily disable SELinux beforehand
-    exec { 'pre-install-disable-selinux' :
-      path => '/usr/bin:/bin:/usr/sbin',
-      provider => 'shell',
-      command => "bash -c 'setenforce 0'",
-      require => Package['sysconfig-pack'],
-    }
-    # setup the zend repo file
+      # temporarily disable SELinux beforehand
+      exec { 'pre-install-disable-selinux' :
+        path => '/usr/bin:/bin:/usr/sbin',
+        provider => 'shell',
+        command => "bash -c 'setenforce 0'",
+        require => Package['sysconfig-pack'],
+      }->
+      # setup the zend repo file
       file { 'zend-repo-file':
         name => '/etc/yum.repos.d/zend.repo',
         source => 'puppet:///modules/dozendserver/zend.rpm.repo',
-        require => Exec['pre-install-disable-selinux'],
-      }    
+      }->  
       # install zend web server after file
       package { 'zend-web-pack':
         name => ['zend-server-ce-php-5.3'],
         ensure => 'present',
-        require => File['zend-repo-file'],
+      }->
+      # stop zendserver, fix then re-enable SELinux
+      exec { 'zend-selinux-fix' :
+        path => '/usr/bin:/bin:/usr/sbin',
+        command => "/usr/local/zend/bin/zendctl.sh stop && semanage port -d -p tcp 10083 && semanage port -a -t http_port_t -p tcp 10083 && semanage port -m -t http_port_t -p tcp 10083 && execstack -c /usr/local/zend/lib/apache2/libphp5.so /usr/local/zend/lib/libssl.so.0.9.8 /usr/lib64/libclntsh.so.11.1 /usr/lib64/libnnz11.so /usr/local/zend/lib/libcrypto.so.0.9.8 /usr/local/zend/lib/debugger/php-5.*.x/ZendDebugger.so /usr/local/zend/lib/php_extensions/curl.so && chcon -R -t httpd_log_t /usr/local/zend/var/log && chcon -R -t httpd_tmp_t /usr/local/zend/tmp && chcon -R -t tmp_t /usr/local/zend/tmp/pagecache /usr/local/zend/tmp/datacache && chcon -t textrel_shlib_t /usr/local/zend/lib/apache2/libphp5.so /usr/lib*/libclntsh.so.11.1 /usr/lib*/libociicus.so /usr/lib*/libnnz11.so && setsebool -P httpd_can_network_connect 1 && setenforce 1 && touch ${notifier_dir}/puppet-dozendserver-selinux-fix",
+        creates => "${notifier_dir}/puppet-dozendserver-selinux-fix",
+        before => Service['zend-server-startup'],
+      }->
+      # make log dir fix permanent to withstand a relabelling
+      exec { 'zend-selinux-log-permfix' :
+        path => '/usr/bin:/bin:/usr/sbin',
+        command => "semanage fcontext -a -t httpd_log_t '/usr/local/zend/var/log(/.*)?'",
+        before => Service['zend-server-startup'],
       }
-    # stop zendserver, fix then re-enable SELinux
-    exec { 'zend-selinux-fix' :
-      path => '/usr/bin:/bin:/usr/sbin',
-      command => "/usr/local/zend/bin/zendctl.sh stop && semanage port -d -p tcp 10083 && semanage port -a -t http_port_t -p tcp 10083 && semanage port -m -t http_port_t -p tcp 10083 && execstack -c /usr/local/zend/lib/apache2/libphp5.so /usr/local/zend/lib/libssl.so.0.9.8 /usr/lib64/libclntsh.so.11.1 /usr/lib64/libnnz11.so /usr/local/zend/lib/libcrypto.so.0.9.8 /usr/local/zend/lib/debugger/php-5.*.x/ZendDebugger.so /usr/local/zend/lib/php_extensions/curl.so && chcon -R -t httpd_log_t /usr/local/zend/var/log && chcon -R -t httpd_tmp_t /usr/local/zend/tmp && chcon -R -t tmp_t /usr/local/zend/tmp/pagecache /usr/local/zend/tmp/datacache && chcon -t textrel_shlib_t /usr/local/zend/lib/apache2/libphp5.so /usr/lib*/libclntsh.so.11.1 /usr/lib*/libociicus.so /usr/lib*/libnnz11.so && setsebool -P httpd_can_network_connect 1 && setenforce 1 && touch ${notifier_dir}/puppet-dozendserver-selinux-fix",
-      require => Package['zend-web-pack'],
-      creates => "${notifier_dir}/puppet-dozendserver-selinux-fix",
-      before => Service['zend-server-startup'],
-    }
-    # make log dir fix permanent to withstand a relabelling
-    exec { 'zend-selinux-log-permfix' :
-      path => '/usr/bin:/bin:/usr/sbin',
-      command => "semanage fcontext -a -t httpd_log_t '/usr/local/zend/var/log(/.*)?'",
-      require => Exec['zend-selinux-fix'],
-      before => Service['zend-server-startup'],
-    }
-    # install SSH2
-    package { 'php-5.3-ssh2-zend-server':
-      ensure => 'present',
-      require => Package['zend-web-pack'],
-      before => Service['zend-server-startup'],
-    }
-# @todo remove this, so long as we get the pecl extensions with the 5.3-zend...
-#     if ($with_memcache) {
-#       package { 'php-pecl-memcache' :
-#         name => ['php-pecl-memcache', 'php-pecl-memcached'],
-#         ensure => 'present',
-#         require => Package['zend-web-pack'],
-#         before => Service['zend-server-startup'],     
-#       }
-#     }
+      # install SSH2
+      package { 'php-5.3-ssh2-zend-server':
+        ensure => 'present',
+        require => Package['zend-web-pack'],
+        before => Service['zend-server-startup'],
+      }
       # install mod SSL
       package { 'apache-mod-ssl' :
         name => ['mod_ssl'],
